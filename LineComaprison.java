55ff1ba4ca9cf217fa48268c3f158b7df2431d8f
Atharva <atharva.tathe55@gmail.com>
2021-04-01 21:37:07 +0530

    Check Two Line equality using equals Method

--- LineComaprison.java
+++ LineComaprison.java
@@ -1,7 +1,8 @@
 import java.lang.Math;
 public class LineComaprison
 {
-		public static void main(String[] args)
+	
+	public static void main(String[] args)
 	{
 
 		int x1=2;
@@ -9,8 +10,25 @@ public class LineComaprison
 		int y1=2;
 		int y2=4;
 
-		double LengthOfLine = Math.sqrt((x2-x1)^2 + (y2-y1)^2);
-		System.out.println("Length of line is: " +LengthOfLine);
+		int x3=2;
+		int x4=5;
+		int y3=2;
+		int y4=4;
+
+		double LengthOfLine1 = Math.sqrt((x2-x1)^2 + (y2-y1)^2);
+		System.out.println("Length of line 1 is: " +LengthOfLine1);
+
+		double LengthOfLine2 = Math.sqrt((x4-x3)^2 + (y4-y3)^2);
+		System.out.println("Length of line 2 is: " +LengthOfLine2);
+
+		Double obj1 = new Double(LengthOfLine1);
+		Double obj2 = new Double(LengthOfLine2);
+
+		if(obj1.equals(obj2))
+			System.out.println("Both lines are equals");
+		else
+			System.out.println("Both line are different");
+
 	}
 }
 
